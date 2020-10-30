b210b26f2b820f1757a68c7a8eabd24041dac8fd
connorshea15 <connorshea15@gmail.com>
2020-10-29 18:56:53 -0700

    integrate ApolloProvider into App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
+import React from 'react';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
 import './App.css';
 import Header from './components/Header';
+import BandForm from './components/BandForm';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3002/graphql'
+});
 
 function App() {
   return (
-    <div>
-      <Header />
-      <header>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        <Header />
+        <header>
+        </header>
+        <BandForm />
+      </div>
+    </ApolloProvider>
   );
 }
 
